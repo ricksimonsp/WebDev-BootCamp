3429da32ad545b41a8b3158251805a082711d39b
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-10-05 18:49:04 -0400

    working on js

--- Running_Js_Script/Conditionals/js/script.js
+++ Running_Js_Script/Conditionals/js/script.js
@@ -8,8 +8,7 @@
 //
 //console.log(random);
 //
-//
-//
+
 //
 //const dayOfWeek = prompt("Enter a day: ").toLowerCase();
 //
@@ -30,19 +29,36 @@
 //}
 
 
+//const age = 89;
+//if (age < 5) {
+//    console.log("You are a baby. You get in for free!")
+//}
+//else if (age < 10) {
+//    console.log("You are a child. You pay $10!")
+//}
+//else if (age < 65) {
+//    console.log("Your are an adult. You pay $20!")
+//}
+//
+//else {
+//    console.log("You are a senior. You pay $10!")
+//}
 
 
-const age = 89;
-if (age < 5) {
-    console.log("You are a baby. You get in for free!")
-}
-else if (age < 10) {
-    console.log("You are a child. You pay $10!")
-}
-else if (age < 65) {
-    console.log("Your are an adult. You pay $20!")
+const password = prompt("Please enter a new password");
+
+if (password.length >= 6) {
+    if (password.indexOf(' ') === -1) {
+        console.log("Valid Password!")
+    }
+    
+    else {
+        console.log("Password cannot contain spaces!")
+    }
 }
 
 else {
-    console.log("You are a senior. You pay $10!")
+    console.log("Password too short! Must be 6+ characters!")
 }
+
+
