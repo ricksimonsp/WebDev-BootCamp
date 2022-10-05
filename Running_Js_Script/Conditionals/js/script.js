a27d5ef736b0302eabf97b1deee926eb12992f75
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-10-04 23:03:06 -0400

    js good for now

--- Running_Js_Script/Conditionals/js/script.js
+++ Running_Js_Script/Conditionals/js/script.js
@@ -1,10 +1,9 @@
 let random = Math.random();
 if (random < 0.5) {
-    console.log("Your number is less than 0.5!! Yay!!")
+    console.log("Your number is less than 0.5")
     console.log(random);
 }
 
-if (random >= 0.5) {
-    console.log("Your number is greater than 0.5!! Holly!!")
-    console.log(random);
+function isEven(num) {
+    
 }
