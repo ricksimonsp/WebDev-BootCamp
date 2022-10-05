6daee8a4bde9be0e6a828875a7a87b529d453101
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-10-05 00:29:11 -0400

    else if and else, done! or almost!

--- Running_Js_Script/Conditionals/js/script.js
+++ Running_Js_Script/Conditionals/js/script.js
@@ -1,9 +1,48 @@
-let random = Math.random();
-if (random < 0.5) {
-    console.log("Your number is less than 0.5")
-    console.log(random);
+//let random = Math.random();
+//if (random < 0.5) {
+//    console.log("Your number is less than 0.5!!")
+//}
+//else {
+//    console.log("Your number is greater (or equal) than 0.5!!")
+//}
+//
+//console.log(random);
+//
+//
+//
+//
+//const dayOfWeek = prompt("Enter a day: ").toLowerCase();
+//
+//if (dayOfWeek === 'monday') {
+//   console.log("Ughhh! I hate Mondays!!!")
+//}
+//
+//else if (dayOfWeek === 'saturday') {
+//    console.log("Yay!! I love Saturdays!")
+//}
+//
+//else if (dayOfWeek === 'riday') {
+//    console.log("Fridays are awsome!!")
+//}
+//
+//else {
+//    console.log("Meh!")
+//}
+
+
+
+
+const age = 89;
+if (age < 5) {
+    console.log("You are a baby. You get in for free!")
+}
+else if (age < 10) {
+    console.log("You are a child. You pay $10!")
+}
+else if (age < 65) {
+    console.log("Your are an adult. You pay $20!")
 }
 
-function isEven(num) {
-    
+else {
+    console.log("You are a senior. You pay $10!")
 }
