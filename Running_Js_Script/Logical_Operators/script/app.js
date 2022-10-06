7907655a270146fb67508f716e8772f27d7affb1
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-10-05 21:40:17 -0400

    Done for today!

---
+++ Running_Js_Script/Logical_Operators/script/app.js
@@ -0,0 +1,28 @@
+//const password = prompt("Enter your password:");
+//if (password.length >= 6 && password.indexOf(' ') === -1){//
+//    console.log("Valid Password!")
+//}
+//else {
+//    console.log("Bad Password!")
+//}
+
+//const age = 73;
+//if(age >= 0 && age < 5 || age >= 65){
+//    console.log("FREE!");
+//}
+//else if (age >=5 && age < 10) {
+//    console.log("$10")
+//}
+//else if (age >= 10 && age < 65) {
+//    console.log("$20")
+//}
+//
+//else {
+//    console.log("Invalide Age!! You asshole!!");
+//}
+
+let firstName = prompt("Enter your first name: ");
+if (!firstName){
+    firstName = prompt("Try again you idiot!!");
+}
+
---
+++ Running_Js_Script/Truthy_Falsy/js/script.js
@@ -0,0 +1,15 @@
+//const userInput = prompt("Enter something: ");
+//
+//if (userInput) {
+//    console.log("TRUTHY!!")
+//}
+//else {
+//    console.log("FALSY!!")
+//}
+
+if (' '){
+console.log("Truth")
+}
+else {
+    console.log("Falsy")
+}
