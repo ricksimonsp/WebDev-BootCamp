a6898840b0eba89538496f5dfccba553db5f1bb1
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-10-11 21:14:42 -0400

    JS is not easy!

---
+++ Running_Js_Script/TODO_List_Project/script/todos.js
@@ -0,0 +1,21 @@
+let input = prompt("What would you like to do?");
+const todos = ['Collect Chicken Eggs', 'Clean Litter Box'];
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list'){
+        console.log('************************')
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('************************')
+    } else if (input === 'new') {
+        const newTodo = prompt('Ok, what is the new todo?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list`);
+    } else if (input === 'delete') {
+        const index = parseInt(prompt('Ok, enter an index to delete:'));
+        const deleted = todos.splice(index, 1);
+        console.log(`Ok, deleted ${deleted[0]}`);
+    }
+    input = prompt("What would you like to do?")
+}
+console.log("Ok, see you!");
