065cd6d72b889df393993f3d59f6d2716e9d7382
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-10-13 22:55:07 -0400

    Functions, ok for today!

--- Running_Js_Script/Functions/script/app.js
+++ Running_Js_Script/Functions/script/app.js
@@ -1,29 +1,33 @@
-function singSong() {
+/*function singSong() {
     console.log("DO");
     console.log("RE");
     console.log("MI");
 }
 
-/*function greet(firstName, lastName) {
-    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
-}
 
-function repeat(str, numTimes) {
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+}*/
+
+/*function repeat(str, numTimes) {
     let result = '';
     for (let i = 0; i < numTimes; i++) {
         result += str;
     }
     console.log(result);
-}
+}*/
+
+
 
 function add(x, y) {
     if (typeof x !== 'number' || typeof y !== 'number') {
         return false;
     }
     return x + y;
-}*/
+}
 
 
+ /*singSong()
  singSong()
  singSong()
  singSong()
@@ -33,6 +37,7 @@ function add(x, y) {
  singSong()
  singSong()
  singSong()
- singSong()
- singSong()
+ singSong()*/
+
+
 
