a1207ae3e098ab3718f82bd014f7abd6434cc5b3
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-10-17 19:33:30 -0400

    Functions Scope done!

---
+++ Running_Js_Script/FunctionScope/script/app.js
@@ -0,0 +1,27 @@
+/*let bird = 'Scarlet Macaw';
+function birdWatch() {
+    let bird = 'Great Blue Heron';
+    console.log(bird);    
+}
+console.log(bird);
+birdWatch();
+
+
+
+function bankRobbery() {
+    const heroes = ['Spiderman', 'Wolverine', 'Black Panther', 'Batwoman'];
+    function cryForHelp() {
+        for (let hero of heroes) {
+            console.log(`Please help us!, ${hero.toUpperCase()}`)
+        }
+    }
+    cryForHelp();
+} */
+
+
+const square = function (num) {
+    return num * num;
+}
+
+square(9);
+
