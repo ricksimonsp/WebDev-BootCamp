a1106fef515f7f38b143b36c963ee7be08a408c0
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-10-27 23:14:54 -0400

    Good for today!

---
+++ Running_Js_Script/TheEventObject/script/app.js
@@ -0,0 +1,40 @@
+/*document.querySelector('button').addEventListener('click', function(evt){
+    console.log(evt)
+})
+
+const input = document.querySelector('input');
+input.addEventListener('keydown', function(e){
+    console.log(e.key)
+    console.log(e.code)
+})
+
+input.addEventListener('keyup', function(){
+    console.log('KEYUP')
+})
+
+window.addEventListener('keydown', function(e){
+    switch (e.code) {
+        case 'ArrowUp':
+            console.log('UP');
+            break;
+        case 'ArrowDown':
+            console.log('DOWN');
+            break;
+        case 'ArrowLeft':
+            console.log('LEFT');
+            break;
+        case 'ArrowRight':
+            console.log('RIGHT');
+            break
+        default:
+            console.log('IGNORED!')
+    }
+})*/
+
+const form = document.querySelector('#shelterForm');
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('SUBMITED THE FORM!')
+})
+
+
