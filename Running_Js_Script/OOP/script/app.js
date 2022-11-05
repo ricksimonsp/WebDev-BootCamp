a40f2510445a38a44c3eff3166911fbfd9f0d513
Ricardo Simone <111938106+ricksimonsp@users.noreply.github.com>
2022-11-05 15:10:39 -0400

    OOP - updated

---
+++ Running_Js_Script/OOP/script/app.js
@@ -0,0 +1,10 @@
+/*String.prototype.yell = function() {
+    return `OMG!!! ${this.toUpperCase()}!!!!!! ARGH!!!!!!!`
+};
+
+Array.prototype.pop = function() {
+    return 'SORRY I WANT THAT ELEMENT!! I WILL NEVER POP IT OFF!';
+}; */
+
+
+
